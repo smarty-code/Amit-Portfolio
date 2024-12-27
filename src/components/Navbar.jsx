14990c1ea51524de2ea54f3df8402df1265bae8f
smarty-code <smartyamit067@gmail.com>
2024-12-27 16:28:30 +0530

    update dependencies and refactor Navbar component for improved styling and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,28 @@
-import React from "react";
 import "../index.css";
+import logo from "../assets/logo.svg";
 
 function Navbar() {
-  function handleClick() {}
-
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-white">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="src\assets\logo.svg" class="h-8" alt=" Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">
+      <nav className="bg-white border-gray-200 dark:bg-white">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-8" alt=" Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">
               Portfolio
             </span>
           </a>
-          <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              class=" hover:bg-slate-600 text-white bg-black  hover:bg-black-500 focus:ring-4 focus:outline-none  font-medium rounded text-sm px-4 py-2 text-center dark:bg-black  "
+              className=" hover:bg-slate-600 text-white bg-black  hover:bg-black-500 focus:ring-4 focus:outline-none  font-medium rounded text-sm px-4 py-2 text-center dark:bg-black  "
             >
               Resume
             </button>
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
               aria-controls="navbar-cta"
               aria-expanded="false"
               onClick={() => {
@@ -33,9 +31,9 @@ function Navbar() {
                 navbar.classList.toggle("block");
               }}
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="w-5 h-5"
+                className="w-5 h-5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -43,23 +41,23 @@ function Navbar() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
             </button>
           </div>
           <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white dark:border-gray-700">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white dark:border-gray-700">
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
+                  className="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
                   aria-current="page"
                 >
                   Home
@@ -68,7 +66,7 @@ function Navbar() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
+                  className="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
                 >
                   About
                 </a>
@@ -76,7 +74,7 @@ function Navbar() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
+                  className="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
                 >
                   Services
                 </a>
@@ -84,7 +82,7 @@ function Navbar() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
+                  className="block py-2 px-3 md:p-0 text-black  rounded md:bg-transparent md:text-black md:dark:text-black"
                 >
                   Contact
                 </a>
